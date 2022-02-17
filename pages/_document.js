fe85e70809d406e498907fd51a13780dfc3f707a
MD. ABU SAYED <hi@abusayed.dev>
2022-02-17 19:39:46 +0600

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -35,7 +35,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Atma:wght@400..700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Atma&display=swap"
             rel="stylesheet"
           />
         </Head>
