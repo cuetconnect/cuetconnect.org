e554c5377379bf9bb596c3f84d71dfc266bdb793
MD. ABU SAYED <hi@abusayed.dev>
2022-02-19 22:30:57 +0600

    sayed

--- components/navbar.js
+++ components/navbar.js
@@ -3,8 +3,6 @@ import ThemeChanger from "./DarkSwitch";
 import { Disclosure } from "@headlessui/react";
 
 export default function Navbar() {
-  const navigation = ["Notices", "Blogs", "Forum", "Materials", "Alumni"];
-
   return (
     <div className="w-full">
       <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
@@ -54,20 +52,16 @@ export default function Navbar() {
                 </Disclosure.Button>
 
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
-                  <>
-                    {navigation.map((item, index) => (
-                      <Link key={index} href="/">
-                        <a className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700">
-                          {item}
-                        </a>
-                      </Link>
-                    ))}
-                    <Link href="/">
-                      <a className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">
-                        Get Started
-                      </a>
-                    </Link>
-                  </>
+                  <Link href="https://notices.cuetconnect.org/">
+                    <a className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700">
+                      Notices
+                    </a>
+                  </Link>
+                  <Link href="https://forum.cuetconnect.org/">
+                    <a className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700">
+                      Forum
+                    </a>
+                  </Link>
                 </Disclosure.Panel>
               </div>
             </>
@@ -77,21 +71,22 @@ export default function Navbar() {
         {/* menu  */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
-            {navigation.map((menu, index) => (
-              <li className="mr-3 nav__item" key={index}>
-                <Link href="/">
-                  <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none">
-                    {menu}
-                  </a>
-                </Link>
-              </li>
-            ))}
+            <li className="mr-3 nav__item">
+              <Link href="https://notices.cuetconnect.org/">
+                <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none">
+                  Notices
+                </a>
+              </Link>
+              <Link href="https://forum.cuetconnect.org/">
+                <a className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none">
+                  Forum
+                </a>
+              </Link>
+            </li>
           </ul>
         </div>
 
         <div className="hidden mr-3 space-x-3 lg:flex nav__item">
-
-
           <ThemeChanger />
         </div>
       </nav>
