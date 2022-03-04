188cb1699533ee5d18dae1b74582a7a268af633d
MD. ABU SAYED <hi@abusayed.dev>
2022-03-04 22:38:45 +0600

    Create sayed.js

---
+++ pages/sayed.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Hero from "../components/hero";
+import Navbar from "../components/navbar";
+import SectionTitle from "../components/sectionTitle";
+import Footer from "../components/footer";
+import Faq from "../components/faq";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>CUET CONNECT💐</title>
+        <meta
+          name="description"
+          content="Connecting CUETians with each other through events, workshops, and competitions."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
