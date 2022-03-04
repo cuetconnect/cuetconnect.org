3012f3af9b90e693a6d8b09a0fe157e275676817
MD. ABU SAYED <hi@abusayed.dev>
2022-03-05 00:47:08 +0600

    up!

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Navbar from "../components/navbar";
 import SectionTitle from "../components/sectionTitle";
 import Footer from "../components/footer";
 import Faq from "../components/faq";
+import Sites from "../components/sites";
+
 
 export default function Home() {
   return (
@@ -19,6 +21,11 @@ export default function Home() {
 
       <Navbar />
       <Hero />
+      <SectionTitle pretitle="CUET CONNECT" title="CUETCONNECT.ORG">
+        How CUET Connect is helping CUETians/non-CUETians!
+      </SectionTitle>
+      <Sites/>
+    
       <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
         Some common questions about CUETConnect💐
       </SectionTitle>
